1b29c005a1715fae09186f1fa1471ea0fd3e53bd
DmytroMartynovCodeCare <dmytro.m@code-care.pro>
2025-03-17 14:16:37 +0200

    W5SMTLAB-15 Frontend init - added axios.js, vuetify.js to plugins, main.js setup

---
+++ src/plugins/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:80', // change to .env
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  install: (app) => {
+    app.config.globalProperties.$axios = apiClient // Доступ через this.$axios
+    app.provide('axios', apiClient) // Доступ через inject('axios')
+  }
+}
