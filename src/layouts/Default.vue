6e04582dcba21cbf9a071831aac87874aed16ea5
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-23 16:44:03 +0200

    default-layout-setup - navbar adjusted

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,19 @@
 <template>
   <v-layout>
-    <v-app-bar app color="primary">
-      <v-app-bar-nav-icon class="d-md-none" />
+    <v-app-bar app color="primary" class="d-flex justify-space-between align-center">
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon class="d-md-none me-2" />
+      </div>
+      <v-spacer />
+      <v-btn to="/login" class="bg-secondary rounded-pill">
+        Sign In
+      </v-btn>
     </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
     <v-footer app color="primary" class="px-2 d-flex justify-space-between align-center">
-      <span class="text-caption">&copy; WStreet Smart Lab</span>
+      <span class="text-caption">&copy; wstreet smart lab</span>
       <div class="d-flex gap-2">
         <v-icon>mdi-linkedin</v-icon>
         <v-icon>mdi-instagram</v-icon>
