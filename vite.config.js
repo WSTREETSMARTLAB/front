01f9b1009f64823aa36bfffbc6baa773072608ca
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-23 09:12:28 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+      vuetify({autoImport: true})
+  ],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true
+  },
+})
