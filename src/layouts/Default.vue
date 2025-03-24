9ace43f076dc4efdf8c032d83cc770938d1d53bc
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-24 10:12:49 +0200

    home-page-setup - added logo, main page styling

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -42,7 +42,7 @@
     <v-footer app color="primary" class="px-2 d-flex justify-space-between align-center">
       <span class="text-caption text-beige">&copy; wstreet smart lab</span>
       <div class="d-flex gap-2">
-        <img class="mr-1" :src="'/icons/W5SMTLogo.svg'" alt="Logo" style="width: 24px; height: 24px;" />
+        <img class="mr-1" :src="W5SMTLogo" alt="Logo" style="width: 24px; height: 24px;" />
         <v-icon class="text-beige mr-1">mdi-linkedin</v-icon>
         <v-icon class="text-beige mr-1">mdi-instagram</v-icon>
       </div>
@@ -51,10 +51,13 @@
 </template>
 
 <script>
+import W5SMTLogo from '@/assets/icons/W5SMTLogo.svg'
+
 export default {
   name: 'Default',
   data () {
     return {
+      W5SMTLogo,
       menuOpen: false
     }
   }
--- src/modules/Home/views/Home.vue
+++ src/modules/Home/views/Home.vue
@@ -1,14 +1,77 @@
 <template>
   <v-container class="pa-0 fill-height">
 
+    <v-container class="intro-block text-center">
+      <div class="intro-content">
+        <div class="intro-top">
+          <img :src="FullLogo" alt="WSTREET SMART LAB logo" />
+        </div>
+
+        <!-- Текст -->
+        <div class="intro-middle">
+          <h1 class="title">WSTREET SMART LAB</h1>
+          <p class="subtitle">We love technologies</p>
+        </div>
+      </div>
+    </v-container>
+
   </v-container>
 </template>
 
 <script>
+import FullLogo from "@/assets/icons/FullLogo.svg"
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      FullLogo,
+    }
+  }
 }
 </script>
 
 <style scoped>
+.intro-block {
+  //padding: 64px 16px 32px;
+  .intro-content {
+    display: grid;
+    height: 100vh;
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+    padding: 24px;
+
+    @media (min-width: 600px) {
+      max-width: 500px;
+    }
+
+    @media (min-width: 960px) {
+      max-width: 960px;
+    }
+  }
+
+  .intro-top img {
+    max-width: 100%;
+    height: auto;
+    max-height: 180px;
+  }
+
+  .intro-middle {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .title {
+      font-size: 2rem;
+      color: $primary-color;
+      font-weight: 700;
+      margin-bottom: 8px;
+    }
+
+    .subtitle {
+      font-size: 1.1rem;
+      color: $dark-gray;
+    }
+  }
+}
 </style>
